a99e9b9f30a816bdf597f279f7e5494dab0af479
Priti Modanwal <pritivns612@gmail.com>
2023-06-07 23:17:18 +0530

    Frontend part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'my-app';
-  
+ public name :string = "";
+ public password :string = "";
+  public store: number[] = [];
 
+  constructor() {
+   
+   }
+
+  ngOnInit() {
+    console.log(this.name,"name is called")
+  }
+  submitForm() {
+    console.log(this.name,"name is called")
+    const payload = {
+      name: this.name,
+      password: this.password
+    }
+    console.log(payload,"payload is called")
+  }
 }
