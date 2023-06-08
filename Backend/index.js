57057e346c544568fc527f7fff430111bb473b98
Priti Modanwal <pritivns612@gmail.com>
2023-06-08 14:48:23 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,36 @@
 const express =  require('express');
 const app = express();
+const connectDB = require('./mongodb');
 const dotenv = require('dotenv').config()
 const port = process.env.PORT;        
-const url = process.env.MONGO_URL;       
-
+const url = process.env.MONGO_URL;        
 app.use(express.json())              //middleware to convert chunks data to json format; to avoid data+=chunk in node
 app.use(express.urlencoded({extended: true}));
+const User = require('./models/userSchema');
 
-const mongoose = require("mongoose"); //importing mongoose module
-mongoose.connect("mongodb://0.0.0.0/newdb"); //database link(local or online url)
+// Handle GET request to fetch all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
+// Handle POST request to create a new user
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
-const userRouter = require('./Routes/users');
-app.use('',userRouter) //first argument is common path, i.e Any url that starts with /users add all of these routes to the end of it
-app.listen(port);
-
-// we did not use the path /user, /user/new & /user/data here because router understands that it's common & there's no need to repeat it.
+connectDB()
+app.listen(port || 5002,()=>{
+  console.log('server started on port', port)
+});
+// app.listen(5001);
